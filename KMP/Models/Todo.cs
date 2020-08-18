cfe32b3b5349cb3d4ea7f66606b786a2361e963c
M Andriansyah Nurcahya <4andriansyah@gmail.com>
2020-08-18 13:32:18 +0700

    Scaffolding db

---
+++ KMP/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KMP.Models
+{
+    public partial class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte Percentage { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Updated { get; set; }
+        public DateTimeOffset? Expiry { get; set; }
+    }
+}
---
+++ KMP/Models/kmpContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace KMP.Models
+{
+    public partial class kmpContext : DbContext
+    {
+        public kmpContext()
+        {
+        }
+
+        public kmpContext(DbContextOptions<kmpContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySQL("Name=KMPMySQL");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.ToTable("todo");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11) unsigned");
+
+                entity.Property(e => e.Created)
+                    .HasColumnName("created")
+                    .HasDefaultValueSql("'''0000-00-00 00:00:00'''");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Expiry)
+                    .HasColumnName("expiry")
+                    .HasDefaultValueSql("'NULL'")
+                    .ValueGeneratedOnAddOrUpdate();
+
+                entity.Property(e => e.Percentage)
+                    .HasColumnName("percentage")
+                    .HasColumnType("tinyint(4)");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasColumnName("title")
+                    .HasMaxLength(25)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("''''''");
+
+                entity.Property(e => e.Updated)
+                    .HasColumnName("updated")
+                    .HasDefaultValueSql("'current_timestamp()'");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
